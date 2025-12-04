3601a87451cc6580941c21117e285f430eac3d44
Volodymyr Kryvytskyi <faleonfall@gmail.com>
2025-12-04 22:44:38 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Planedoro/>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import Planedoro from "./components/Planedoro.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {Planedoro},
+});
+</script>
+
+<style scoped>
+</style>
